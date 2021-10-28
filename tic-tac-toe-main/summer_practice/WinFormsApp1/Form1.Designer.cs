3ff0251ad14d8f8c2089a6ecb6969a49ba31c39e
nick-wow <71384326+nick-wow@users.noreply.github.com>
2021-10-28 10:06:37 +0300

    Add files via upload

---
+++ tic-tac-toe-main/summer_practice/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 91);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(579, 104);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(113, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Новая игра";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(523, 45);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(241, 28);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Сохранить результат(ы) игр(ы) и выйти";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 91);
+            this.button2.TabIndex = 12;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(321, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 91);
+            this.button3.TabIndex = 13;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(36, 159);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 91);
+            this.button4.TabIndex = 14;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(181, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 91);
+            this.button5.TabIndex = 15;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(321, 159);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 91);
+            this.button6.TabIndex = 16;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(36, 275);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 91);
+            this.button7.TabIndex = 17;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(181, 275);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 91);
+            this.button8.TabIndex = 18;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(321, 275);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 91);
+            this.button9.TabIndex = 19;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 594);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "i";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ tic-tac-toe-main/summer_practice/WinFormsApp1/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter SW = new StreamWriter(new FileStream("score.txt", FileMode.Open, FileAccess.Write)); //Создаём файл (если его нет) и открываем
+        string x_win = "Победили X";
+        string o_win = "Победили O";
+
+        string X = "";
+        Random rnd = new Random();
+        int[] Arr_1 = new int[9];
+        Button[] btn = new Button[9];
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (rnd.Next(1, 10) <= 5)
+            {
+                X = "Крестики";
+                label1.Text = "Ходят X";
+            }
+
+            else
+            {
+                X = "Нолики";
+                label1.Text = "Ходят O";
+            }
+
+            btn[0] = button1;
+
+            btn[1] = button2;
+
+            btn[2] = button3;
+
+            btn[3] = button4;
+
+            btn[4] = button5;
+
+            btn[5] = button6;
+
+            btn[6] = button7;
+
+            btn[7] = button8;
+
+            btn[8] = button9;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (X == "Крестики")
+            {
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    if (sender == btn[i])
+                    {
+                        btn[i].Text = "X";
+                        label1.Text = "Ходят O";
+                        X = "Нолики";
+                        btn[i].Enabled = false;
+                        Arr_1[i] = 2;
+                        break;
+                    }
+                }
+            }
+            else    //Нолики
+            {
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    if (sender == btn[i])
+                    {
+                        btn[i].Text = "O";
+                        label1.Text = "Ходят X";
+                        X = "Крестики";
+                        btn[i].Enabled = false;
+                        Arr_1[i] = 1;
+                        break;
+                    }
+                }
+            }
+
+            //Условия для крестиков
+            if ((Arr_1[0] == 2) && (Arr_1[1] == 2) && (Arr_1[2] == 2))
+            {
+                btn[0].BackColor = System.Drawing.Color.Red;
+                btn[1].BackColor = System.Drawing.Color.Red;
+                btn[2].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            else if ((Arr_1[3] == 2) && (Arr_1[4] == 2) && (Arr_1[5] == 2))
+            {
+                btn[3].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[5].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            else if ((Arr_1[6] == 2) && (Arr_1[7] == 2) && (Arr_1[8] == 2))
+            {
+                btn[6].BackColor = System.Drawing.Color.Red;
+                btn[7].BackColor = System.Drawing.Color.Red;
+                btn[8].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            else if ((Arr_1[0] == 2) && (Arr_1[3] == 2) && (Arr_1[6] == 2))
+            {
+                btn[0].BackColor = System.Drawing.Color.Red;
+                btn[3].BackColor = System.Drawing.Color.Red;
+                btn[6].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            else if ((Arr_1[1] == 2) && (Arr_1[4] == 2) && (Arr_1[7] == 2))
+            {
+                btn[1].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[7].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            else if ((Arr_1[2] == 2) && (Arr_1[5] == 2) && (Arr_1[8] == 2))
+            {
+                btn[2].BackColor = System.Drawing.Color.Red;
+                btn[5].BackColor = System.Drawing.Color.Red;
+                btn[8].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            else if ((Arr_1[0] == 2) && (Arr_1[4] == 2) && (Arr_1[8] == 2))
+            {
+                btn[0].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[8].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            else if ((Arr_1[2] == 2) && (Arr_1[4] == 2) && (Arr_1[6] == 2))
+            {
+                btn[2].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[6].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили X";
+                SW.WriteLine(x_win);
+            }
+
+            //Нолики
+
+            else if ((Arr_1[0] == 1) && (Arr_1[1] == 1) && (Arr_1[2] == 1))
+            {
+                btn[0].BackColor = System.Drawing.Color.Red;
+                btn[1].BackColor = System.Drawing.Color.Red;
+                btn[2].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+            }
+
+            else if ((Arr_1[3] == 1) && (Arr_1[4] == 1) && (Arr_1[5] == 1))
+            {
+                btn[3].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[5].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+                SW.WriteLine(o_win);
+            }
+
+            else if ((Arr_1[6] == 1) && (Arr_1[7] == 1) && (Arr_1[8] == 1))
+            {
+                btn[6].BackColor = System.Drawing.Color.Red;
+                btn[7].BackColor = System.Drawing.Color.Red;
+                btn[8].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+                SW.WriteLine(o_win);
+            }
+
+            else if ((Arr_1[0] == 1) && (Arr_1[3] == 1) && (Arr_1[6] == 1))
+            {
+                btn[0].BackColor = System.Drawing.Color.Red;
+                btn[3].BackColor = System.Drawing.Color.Red;
+                btn[6].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+                SW.WriteLine(o_win);
+            }
+
+            else if ((Arr_1[1] == 1) && (Arr_1[4] == 1) && (Arr_1[7] == 1))
+            {
+                btn[1].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[7].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+                SW.WriteLine(o_win);
+            }
+
+            else if ((Arr_1[2] == 1) && (Arr_1[5] == 1) && (Arr_1[8] == 1))
+            {
+                btn[2].BackColor = System.Drawing.Color.Red;
+                btn[5].BackColor = System.Drawing.Color.Red;
+                btn[8].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+                SW.WriteLine(o_win);
+            }
+
+            else if ((Arr_1[0] == 1) && (Arr_1[4] == 1) && (Arr_1[8] == 1))
+            {
+                btn[0].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[8].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+                SW.WriteLine(o_win);
+            }
+
+            else if ((Arr_1[2] == 1) && (Arr_1[4] == 1) && (Arr_1[6] == 1))
+            {
+                btn[2].BackColor = System.Drawing.Color.Red;
+                btn[4].BackColor = System.Drawing.Color.Red;
+                btn[6].BackColor = System.Drawing.Color.Red;
+
+                for (int i = 0; i < btn.Length; i++)
+                {
+                    btn[i].Enabled = false;
+                }
+                button10.Enabled = true;
+                label1.Text = "Победили O";
+                SW.WriteLine(o_win);
+            }
+
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < btn.Length; i++)
+            {
+                Arr_1[i] = 0;
+                btn[i].Text = "";
+                btn[i].Enabled = true;
+                btn[i].BackColor = System.Drawing.Color.White;
+            }
+            if (rnd.Next(1, 10) <= 5)
+            {
+                X = "Крестики";
+                label1.Text = "Ходят X";
+            }
+            else
+            {
+                X = "Нолики";
+                label1.Text = "Ходят O";
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SW.Close();
+        }
+
+        private void Form1_Closing(object sender, EventArgs e)
+        {
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            SW.Close();
+            Application.Exit();
+        }
+    }
+}
